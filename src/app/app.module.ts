92474753a4062ecd877a31a151c91ade33eed26c
David Lidebrandt <david.lidebrandt@gmail.com>
2021-11-16 00:05:23 +0100

    Add main menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +17,8 @@ import { WindowComponent } from './components/window/window.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/window/window.component.ts
+++ src/app/components/window/window.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-window',
   templateUrl: './window.component.html',
@@ -10,6 +11,42 @@ export class WindowComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hoveredStartGame: boolean = false;
+
+  animateStartGame() {
+    this.hoveredStartGame = !this.hoveredStartGame;
+  }
+
+  hoveredSettings: boolean = false;
+
+  animateSettings() {
+    this.hoveredSettings = !this.hoveredSettings;
+  }
+
+  hoveredHelp: boolean = false;
+
+  animateHelp() {
+    this.hoveredHelp = !this.hoveredHelp;
+  }
+
+  hoveredLeaderBoard: boolean = false;
+
+  animateLeaderBoard() {
+    this.hoveredLeaderBoard = !this.hoveredLeaderBoard;
+  }
+
+  hoveredScores: boolean = false;
+
+  animateScores() {
+    this.hoveredScores = !this.hoveredScores;
+  }
+
+  hoveredLogo: boolean = false;
+
+  animateLogo() {
+    this.hoveredLogo = !this.hoveredLogo;
   }
 
 }
