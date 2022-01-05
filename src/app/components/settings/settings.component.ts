7f12b6b9972a69d57985fd28b56964737ce81ebe
David Lidebrandt <david.lidebrandt@gmail.com>
2022-01-05 15:18:17 +0100

    Create settings service

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsDataService } from 'src/app/services/settings-data.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  soundOn!: boolean;
+  level!: string;
 
-  constructor() { }
+  constructor(private settings: SettingsDataService) {
+    let currentSettings = settings.getSettings(); 
+    this.soundOn = currentSettings["sound"] === "on" ? true : false;
+    this.level = currentSettings["level"];
+  }
 
   ngOnInit(): void {
+  }
+
+  getSettingsFromForm(event:Event) {
+    event.preventDefault();
+  }
+
+  setSettings(sound:string, level:string) {
+
   }
 
 }
--- src/app/services/settings-data.service.ts
+++ src/app/services/settings-data.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
+interface Settings {
+  sound:string,
+  level:string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsDataService {
 
   constructor() { }
+
+  getSettings():Settings {
+    
+    let soundSettingsInLocalStorage = localStorage.getItem("sound");
+    let levelSettingsInLocalStorage = localStorage.getItem("level");
+    let soundSetting = soundSettingsInLocalStorage != null ? soundSettingsInLocalStorage : "off";
+    let levelSetting = levelSettingsInLocalStorage != null ? levelSettingsInLocalStorage : "easy";
+
+    return {
+      sound: soundSetting,
+      level: levelSetting,
+    }
+  }
+
+  setSettings(sound:string, level:string):string {
+    if ((sound !== "off" || "on") || (level !== "easy" || "medium" || "hard")) {
+      return "Error applying settings, faulty values"
+    }
+    localStorage.setItem("sound", sound);
+    localStorage.setItem("level", level);
+    return "Your settings was applied"
+  }
+
+
 }
