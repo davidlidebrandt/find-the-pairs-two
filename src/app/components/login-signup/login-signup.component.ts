2949d1cf6036b3b9726353dc742c2ba9025140c8
David Lidebrandt <david.lidebrandt@gmail.com>
2022-01-07 14:25:44 +0100

    Style login/singup tabs

--- src/app/components/login-signup/login-signup.component.ts
+++ src/app/components/login-signup/login-signup.component.ts
@@ -7,12 +7,21 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login-signup.component.css']
 })
 export class LoginSignupComponent implements OnInit {
-  userName = new FormControl('');
-  password = new FormControl('');
+  logInShown:boolean = true;
+  
+  logInUserName = new FormControl('');
+  logInPassword = new FormControl('');
+  signUpUserName = new FormControl('');
+  signUpPassword = new FormControl('');
+  signUpRepeatPassword = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleLogInShown():void {
+    this.logInShown = !this.logInShown;
   }
 
 }
