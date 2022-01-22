ab9684b9a30dfb643a5fdfd291189383145d173d
David Lidebrandt <david.lidebrandt@gmail.com>
2022-01-22 13:13:57 +0100

    Add input error help text

--- src/app/components/login-signup/login-signup.component.ts
+++ src/app/components/login-signup/login-signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface Error {
+    'text':string,
+    'hidden':boolean
+}
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
@@ -10,6 +14,7 @@ export class LoginSignupComponent implements OnInit {
   constructor() { }
   
   logInShown:boolean = true;
+  
   userNameToolTipHidden:boolean = true;
   passwordToolTipHidden:boolean = true;
   repeatPasswordToolTipHidden = true;
@@ -29,6 +34,51 @@ export class LoginSignupComponent implements OnInit {
   passwordSignUpInputField = this.signUpForm.controls.signUpPassword;
   passwordRepeatSignUpInputField = this.signUpForm.controls.signUpRepeatPassword;
 
+  signUpUserNameHelpText:Error = {
+    text: '6-12 characters, at least one number one lowercase and one uppercase letter',
+    hidden: true
+  }
+
+  signUpPasswordHelpText:Error = {
+    text: '6-12 characters, at least one number one lowercase and one uppercase letter',
+    hidden: true
+  }
+  signUpUserRepeatPasswordHelpText:Error = {
+    text: 'Please enter your chosen password again',
+    hidden: true
+  }
+
+  inputFieldChanged(inputFieldControl: AbstractControl, inputHelpText:Error):void {
+    if(inputFieldControl.value === "" && inputFieldControl === this.passwordRepeatSignUpInputField) {
+      inputHelpText.text = 'Please enter your chosen password again';
+      return
+    } else if(inputFieldControl.value === "") {
+      inputHelpText.text = '6-12 characters, at least one number one lowercase and one uppercase letter';
+      return
+    }
+    if(!inputFieldControl.errors) {
+      inputHelpText.text = "";
+      inputHelpText.hidden = true;
+      return
+    } else inputHelpText.hidden = false;
+    
+    let minLengthError = inputFieldControl.errors?.hasOwnProperty("minlength");
+    let maxLengthError = inputFieldControl.errors?.hasOwnProperty("maxlength");
+    
+    if(minLengthError && inputFieldControl === this.userNameSignUpInputField
+      ) {
+      inputHelpText.text = "Your username is too short"
+    } else if(minLengthError) {
+      inputHelpText.text = "Your password is too short"
+    }
+    
+    if(maxLengthError && inputFieldControl === this.userNameSignUpInputField) {
+      inputHelpText.text = "Your username is too long"
+    } else if(maxLengthError) {
+      inputHelpText.text = "Your password is too long"
+    }
+  }
+
   ngOnInit(): void {
   }
 
@@ -37,15 +87,15 @@ export class LoginSignupComponent implements OnInit {
   }
 
   toggleUsernameToolTip():void {
-    this.userNameToolTipHidden = !this.userNameToolTipHidden;
+    this.signUpUserNameHelpText.hidden = !this.signUpUserNameHelpText.hidden;
   }
 
   togglePasswordToolTip():void {
-    this.passwordToolTipHidden = !this.passwordToolTipHidden;
+    this.signUpPasswordHelpText.hidden = !this.signUpPasswordHelpText.hidden;
   }
 
   toggleRepeatPasswordToolTip():void {
-    this.repeatPasswordToolTipHidden = !this.repeatPasswordToolTipHidden;
+    this.signUpUserRepeatPasswordHelpText.hidden = !this.signUpUserRepeatPasswordHelpText.hidden;
   }
 
 
