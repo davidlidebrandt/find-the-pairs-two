745febfe888fa635f00a85fb0cbaf73b769a31de
David Lidebrandt <david.lidebrandt@gmail.com>
2022-02-07 18:26:50 +0100

    Add router guard game page

---
+++ src/app/services/user-auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { User, UserAuthService } from './user-auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthGuardService implements CanActivate {
+  canActivatePath!:boolean;
+
+  constructor(public authService:UserAuthService, public router:Router) { }
+
+  canActivate():boolean {
+    this.authService.getIfUserIsLoggedIn().subscribe((user)=> {
+      this.canActivatePath = user.isLoggedIn
+    })
+    return this.canActivatePath
+  }
+}
